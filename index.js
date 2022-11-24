419a0ef2a3b4dd2d1fa70531ad102ec29e44fb63
mdYeasin-cmd <yeasin200011@gmail.com>
2022-11-24 18:33:06 +0600

    Initial server setup

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// Databse
+
+app.get('/', (req, res) => {
+    res.send('Iqra Laptop Zone server is running.')
+});
+
+app.listen(port, () => {
+    console.log(`Iqra Laptop Zone server is running on port ${port}`);
+})
