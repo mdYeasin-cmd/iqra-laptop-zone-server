d7b53b726c214ed78f226e3524631c708ddd32cc
mdYeasin-cmd <yeasin200011@gmail.com>
2022-11-25 18:47:21 +0600

    Api created for saved logged in user

--- index.js
+++ index.js
@@ -9,16 +9,23 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
-
 // Databse
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.0nieed1.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
-    try {
 
+    const usersCollection = client.db('iqraLaptopZoneDB').collection('users');
+
+    try {
+        // users related api
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            console.log(user);
+            const result = await usersCollection.insertOne(user);
+            res.send(result);
+        })
     }
     finally {
 
