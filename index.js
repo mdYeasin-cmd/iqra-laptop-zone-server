021e58f665c8f51e34eab8d5daadc129f635d058
mdYeasin-cmd <yeasin200011@gmail.com>
2022-11-27 18:35:02 +0600

    Api created for get seller

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -70,10 +70,38 @@ async function run() {
             res.send(result);
         });
 
-        app.get('/orders', async (req, res) => {
+        app.get('/products', async (req, res) => {
             const email = req.query.email;
-            const query = { email: email };
-            const result = await ordersCollection.find(query).toArray();
+            const query = { sellerEmail: email };
+            const result = await productsCollection.find(query).toArray();
+            res.send(result);
+        });
+
+        app.delete('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id);
+            const query = { _id: ObjectId(id) };
+            const result = await productsCollection.deleteOne(query);
+            res.send(result);
+        })
+
+        app.put('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const body = req.body.isAdvertise;
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: {
+                    isAdvertise: body
+                }
+            };
+            const result = await productsCollection.updateOne(filter, updateDoc, options);
+            res.send(result);
+        });
+
+        app.get('/sellers', async (req, res) => {
+            const query = { userRole: 'seller' };
+            const result = await usersCollection.find(query).toArray();
             res.send(result);
         })
 
